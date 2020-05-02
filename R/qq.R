4aa4800129bc49a77a35f4b8ddbf9143436b3b07
Tobias Iven <tobiasiven@gmail.com>
2020-05-02 00:44:00 -0700

    qq plot using ggplot
    function for getting qqplot from output of lm call

---
+++ R/qq.R
@@ -0,0 +1,34 @@
+#'
+#' @param mod List output from an lm() call
+#'
+#'
+#' @return ggplot object
+#'
+#' @importFrom ggplot2 ggthemes
+#'
+#'
+#' @export
+
+qq <- function(mod){
+  res <- rstandard(mod)
+
+  n = length(res)
+  pquants <- 1:n / n
+  tquants <- qnorm(pquants)
+
+
+  y <- res[order(res, decreasing = T)] #sort x = data values
+  x <- tquants[order(tquants,decreasing = T)] #sort generated values
+
+  qqdf <- data.frame(x,y) #bind together
+
+  qqp <- ggplot(qqdf, aes(x,y, text = row.names(qqdf))) + #x is data values, y is normal values
+    geom_point() +
+    geom_abline() + #fit line y = x
+    xlab("Data Values") +
+    ylab("Theoretical Qunatiles") +
+    ggtitle("Normal QQ Plot") +
+    theme_fivethirtyeight()
+
+  ggplotly(qqp, tooltip = c("Row: ", "text"))
+}

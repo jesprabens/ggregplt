38feb34fac1cea31f1b1ddcb148b6003a61d2caa
maggiegreco <55599545+maggiegreco@users.noreply.github.com>
2020-05-03 14:15:15 -0700

    Made it interactive

--- R/MR.R
+++ R/MR.R
@@ -1,29 +1,26 @@
-#' Calculates VIF and correlation matrix for a given Multiple Regression equation.
+#' Calculates VIF and interactive correlation matrix for a given Multiple Regression equation.
 #'
-#' @param dataset The dataset to be used
-#' @param equation A multiple regression equation supplied by user
+#'
+#' @param eq A multiple regression equation supplied by user
 #'
 #'
 #' @return Plot and VIF
 #'
-#' @importFrom dplyr ggplot2 car reshape2 knitr irr dplyr
+#' @importFrom dplyr ggplot2 car reshape2 knitr irr dplyr viridis ggiraph lmf ggcorrplot plotly
 #' 
 #'
 #' @export
 
 MR <- function(eq){
-  dataset <- eq$model
+  dataset <- eq$model[,-1]
+  corr <- round(cor(dataset), 1)
   VIF <- vif(eq)
-  corr.matrix <- round(cor(data.from.eq),2)
-  corr.matrix.melt <- melt(corr.matrix)
-  heatmap <- ggplot(data = corr.matrix.melt, aes(x=Var1, y=Var2, fill=value)) + 
-    geom_tile() + 
-    scale_fill_continuous(type = "viridis") + 
-    ggtitle("Correlation Matrix")+
-    theme(axis.title.x=element_blank(),
-          axis.title.y=element_blank()) + 
-    labs(fill = "Corr.")
-  heatmap
-    result <- list(heatmap, VIF)
-    return(result)
+  p.mat <- cor_pmat(dataset)
+  corr.plot <- ggcorrplot(
+    corr, hc.order = TRUE, outline.col = "white",
+    p.mat = p.mat, colors = c("#FDE725FF", "#1F968BFF", "#440154FF")) + 
+    ggtitle("Correlation Matrix")
+    result <- list(VIF, ggplotly(corr.plot))
+  return(result)
+  
 }

341e94b38912d40bd4b660375e757000d0017129
jesprabens <jesprabens@users.noreply.github.com>
2020-05-03 15:18:45 -0700

    Added helper function and expanded on original function

--- R/int_reg.R
+++ R/int_reg.R
@@ -9,20 +9,75 @@
 #' @importFrom ggthemes
 #'
 #' @export
-int_reg <- function(mod){
+int_reg <- function(mod, x = NULL){
 
-  df <- data.frame(reg2$model)
+  if (is.null(x) == FALSE){
+
+    df <- data.frame(mod$model)
+
+    names <- colnames(df)
+
+    response <- df[,1]
+
+    for(i in 2:length(df)){
+
+      predictor <- df[,i]
+
+      intercept <- mod$coefficients[1]
+
+      predictor_val <- mod$coefficients[i]
+
+      pred <- (intercept + predictor_val * x)
+
+      p <- df %>%
+        ggplot(aes(x = predictor, y = response)) +
+        geom_point() +
+        geom_smooth(method=lm, se = FALSE) +
+        geom_point(aes(x = x, y = pred, size = 4, color = "red")) +
+        ggtitle(paste("Regression Plot", names[1] ,"vs.", names[i])) +
+        labs(x = names[i],
+             y = names[1]) +
+        theme_fivethirtyeight() +
+        theme(axis.title = element_text(),
+              legend.position = "none")
+
+      p_int <- ggplotly(p)
+
+      print(p_int)
+
+    }
+
+  } else {
+
+    no_pred_plot(mod)
+
+  }
+
+}
+
+
+
+#' Plots the interactive regression with no predicted x
+#'
+#' @param mod The regression model
+#'
+#' @return An interactive plot or plots
+#'
+#' @importFrom plotly ggplotly
+#' @importFrom tidyverse
+#' @importFrom ggthemes
+no_pred_plot <- function(mod){
+
+  df <- data.frame(mod$model)
 
   names <- colnames(df)
 
   response <- df[,1]
 
-  # cycles through each predictor
   for(i in 2:length(df)){
 
     predictor <- df[,i]
 
-    # creates a regression plot, response vs predictor
     p <- df %>%
       ggplot(aes(x = predictor, y = response)) +
       geom_point() +
@@ -31,13 +86,25 @@ int_reg <- function(mod){
       labs(x = names[i],
            y = names[1]) +
       theme_fivethirtyeight() +
-      theme(axis.title = element_text())
+      theme(axis.title = element_text(),
+            legend.position = "none")
 
-    # makes the plots interactive
     p_int <- ggplotly(p)
 
     print(p_int)
 
   }
+
+}
+
+
+
+
+
+
+
+
+
+
 
 }

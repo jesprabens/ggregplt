ce51b9ce13beb240c830279c101d3b5665e4e711
maggiegreco <55599545+maggiegreco@users.noreply.github.com>
2020-05-03 21:13:14 -0700

    Change to make VIF function separate

--- R/MR.R
+++ R/MR.R
@@ -14,13 +14,11 @@
 MR <- function(eq){
   dataset <- eq$model[,-1]
   corr <- round(cor(dataset), 1)
-  VIF <- vif(eq)
   p.mat <- cor_pmat(dataset)
   corr.plot <- ggcorrplot(
     corr, hc.order = TRUE, outline.col = "white",
     p.mat = p.mat, colors = c("#FDE725FF", "#1F968BFF", "#440154FF")) + 
     ggtitle("Correlation Matrix")
-    result <- list(VIF, ggplotly(corr.plot))
+    result <- ggplotly(corr.plot)
   return(result)
   
-}

c8c8f0b3d55bd75607b74a1ee5bde4ea94a2912c
maggiegreco <55599545+maggiegreco@users.noreply.github.com>
2020-05-03 21:16:13 -0700

    Update VIF_table.R

--- R/VIF_table.R
+++ R/VIF_table.R
@@ -1,3 +1,17 @@
+#' Calculates VIF for given multiple regression model
+#'
+#'
+#' @param eq A multiple regression equation supplied by user
+#'.@param limit A user defined limit for VIF value 
+#'
+#' @return VIF with decision if the value is too high or not
+#'
+#' @importFrom dplyr ggplot2 car reshape2 knitr irr dplyr lmf
+#' 
+#'
+#' @export
+
+
 VIF_table <- function(eq, limit){
   VIF <- data.frame(vif(eq))[,1]
   for (i in 1:length(VIF)){ 
